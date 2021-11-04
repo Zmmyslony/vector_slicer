28d3f6aada63226d43a7901fa4a2c7436a42d162
zmmyslony <zmmyslony@gmail.com>
2021-11-04 10:36:42 +0000

    added gitignore to builds

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Work/Cambridge/printer/Vector Slicer
+# Install script for directory: C:/Users/zmmys/CLionProjects/Vector_Slicer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Work/Cambridge/printer/Vector Slicer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/zmmys/CLionProjects/Vector_Slicer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include "./libs/high_level/AutomaticPathGeneration.h"
 
-double VERSION = 0.2;
+double VERSION = 0.1;
 // Best seed out of 200: 193
+
+std::string mainDirectory = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns)";
+
 
 int main() {
     printf("\n\tVector slicer version %.1f.", VERSION);
 
-    std::string radial = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns\radial, r = 1 cm)";
-    std::string azimuthal = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns\azimuthal, r = 1 cm)";
-    std::string diagonal = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns\diagonal, 2x1 cm)";
-    std::string linear = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns\linear, 2x1 cm)";
-    std::string spiral = R"(C:\Work\Cambridge\printer\Vector Slicer Patterns\spiral, r = 1 cm)";
+    std::string radial = mainDirectory + R"(\radial, r = 1 cm)";
+    std::string azimuthal = mainDirectory + R"(\azimuthal, r = 1 cm)";
+    std::string diagonal = mainDirectory + R"(\diagonal, 2x1 cm)";
+    std::string linear = mainDirectory + R"(\linear, 2x1 cm)";
+    std::string spiral = mainDirectory + R"(\spiral, r = 1 cm)";
 
     std::vector<std::string> allPatterns = {radial, azimuthal, diagonal, linear, spiral};
     for (auto& patternType : allPatterns) {
         generatePrintPattern(patternType, 1, 100);
     }
-//    generatePrintPattern(radial, 1, 50);
+//    generatePrintPattern(diagonal, 1, 5);
 //    generatePrintPattern(azimuthal, 1, 100);
 //    generatePrintPatternsMultithread(linear, 1, 5);
     return 0;
