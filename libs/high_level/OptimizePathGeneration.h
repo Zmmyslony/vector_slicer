37de5641dbec6302eeb45587c8dd7bdd42fe71f6
zmmyslony <zmmyslony@gmail.com>
2021-11-05 17:40:18 +0000

    Started writing optimization algorithm for filling the patterns.

---
+++ libs/high_level/OptimizePathGeneration.h
@@ -0,0 +1,14 @@
+//
+// Created by zmmys on 05/11/2021.
+//
+
+#ifndef VECTOR_SLICER_OPTIMIZEPATHGENERATION_H
+#define VECTOR_SLICER_OPTIMIZEPATHGENERATION_H
+
+#include "../auxiliary/FillingConfig.h"
+#include <string>
+
+FillingConfig findBestConfig(const std::string &path, int threads, int iterationDepth, int seed);
+FillingConfig findBestConfig(const std::string &path, int threads, int iterationDepth);
+
+#endif //VECTOR_SLICER_OPTIMIZEPATHGENERATION_H
