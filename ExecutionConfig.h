92e928c4639f1d59949fd741ec249b730c896e73
zmmyslony <zmmyslony@gmail.com>
2022-11-11 16:02:50 +0000

    Added copyright notice

--- ExecutionConfig.h
+++ ExecutionConfig.h
@@ -1,3 +1,15 @@
+// 2022, Michał Zmyślony, mlz22@cam.ac.uk.
+//
+// Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
+//
+// This file is part of Vector Slicer.
+//
+// Vector Slicer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 //
 // Created by Michał Zmyślony on 11/01/2022.
 //
