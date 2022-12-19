5b408d9ebeff8cee3c356c044204c5db8ebed10c
zmmyslony <zmmyslony@gmail.com>
2022-12-19 11:27:36 +0000

    Added recalculation of best configs

--- source/bayesian_optimisation.h
+++ source/bayesian_optimisation.h
@@ -1,3 +1,4 @@
+
 // 2022, Michał Zmyślony, mlz22@cam.ac.uk.
 //
 // Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
@@ -9,30 +10,6 @@
 // Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along with Vector Slicer. If not, see <https://www.gnu.org/licenses/>.
-
-// 2022, Michał Zmyślony, mlz22@cam.ac.uk.
-//
-// Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
-//
-// This file is part of Vector Slicer.
-//
-// Vector Slicer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//
-// Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
-
-// 2022, Michał Zmyślony, mlz22@cam.ac.uk.
-//
-// Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
-//
-// This file is part of Vector Slicer.
-//
-// Vector Slicer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//
-// Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 
 //
 // Created by Michał Zmyślony on 21/11/2022.
@@ -61,5 +38,7 @@ public:
 };
 
 void optimisePattern(const fs::path &pattern_path, int seeds, int threads);
+
+void recalculateBestConfig(const fs::path &pattern_path);
 
 #endif //VECTOR_SLICER_BAYESIAN_OPTIMISATION_H
