5dbdbc08232ae0d2532e578610ac28f2e4f6c1ee
zmmyslony <zmmyslony@gmail.com>
2023-05-11 16:02:39 +0200

    Update of copyright notice

--- source/pattern/auxiliary/configuration_reading.cpp
+++ source/pattern/auxiliary/configuration_reading.cpp
@@ -1,14 +1,19 @@
-// 2023, Michał Zmyślony, mlz22@cam.ac.uk.
+// Copyright (c) 2023, Michał Zmyślony, mlz22@cam.ac.uk.
 //
 // Please cite Michał Zmyślony and Dr John Biggins if you use any part of this code in work you publish or distribute.
 //
 // This file is part of Vector Slicer.
 //
-// Vector Slicer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Vector Slicer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
 //
-// Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// Vector Slicer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with Vector Slicer. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with Vector Slicer.
+// If not, see <https://www.gnu.org/licenses/>.
 
 //
 // Created by Michał Zmyślony on 28/03/2023.
