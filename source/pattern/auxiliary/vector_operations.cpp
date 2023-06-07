dd5e2b039a02a974b2f96ea20c2726fe2bd40f3e
zmmyslony <zmmyslony@gmail.com>
2023-06-07 16:01:23 +0200

    Added more operators on vectors and conversion between vectors and valarrays

--- source/pattern/auxiliary/vector_operations.cpp
+++ source/pattern/auxiliary/vector_operations.cpp
@@ -19,8 +19,12 @@
 // Created by Michał Zmyślony on 21/11/2022.
 //
 
-#include <numeric>
 #include "vector_operations.h"
+
+#include <numeric>
+#include <stdexcept>
+
+#include "valarray_operations.h"
 
 std::vector<std::valarray<int>>
 stitchTwoVectors(std::vector<std::valarray<int>> backwards_vector, std::vector<std::valarray<int>> forwards_vector) {
@@ -36,18 +40,79 @@ stitchTwoVectors(std::vector<std::valarray<int>> backwards_vector, std::vector<s
 
 double mean(const std::vector<double> &data) {
     double sum = std::accumulate(std::begin(data), std::end(data), 0.0);
-    double mean =  sum / data.size();
+    double mean = sum / data.size();
     return mean;
 }
 
 double standardDeviation(const std::vector<double> &data) {
     double mean_value = mean(data);
     long double variance_sum = 0;
-    for (auto &element : data) {
+    for (auto &element: data) {
         long double difference = element - mean_value;
-        variance_sum +=  difference * difference;
+        variance_sum += difference * difference;
     }
-    auto standard_deviation = (double)(sqrt(variance_sum) / data.size());
+    auto standard_deviation = (double) (sqrt(variance_sum) / data.size());
     return standard_deviation;
+}
+
+std::vector<double> itod(const std::vector<int> &vector) {
+    return {vector.begin(), vector.end()};
+}
+
+std::vector<int> dtoi(const std::vector<double> &vector) {
+    return {vector.begin(), vector.end()};
+}
+
+std::vector<double> add(const std::vector<double> &first, const std::vector<double> &second) {
+    if (first.size() == second.size()) {
+        std::vector<double> sum = first;
+        for (int i = 0; i < first.size(); i++) {
+            sum[i] += second[i];
+        }
+        return sum;
+    } else {
+        throw std::runtime_error("Adding two vectors of uneven size");
+    }
+}
+
+vali vectoval(const veci &vec) {
+    return {vec.data(), vec.size()};
+}
+
+vald vectoval(const vecd &vec) {
+    return {vec.data(), vec.size()};
+}
+
+double dot(const vecd &first, const vecd &second) {
+    return dot(vectoval(first), vectoval(second));
+}
+
+double norm(const vecd &vec) {
+    return norm(vectoval(vec));
+}
+
+vecd scale(const vecd &vec, double scale) {
+    vecd vec_rescaled;
+    for (auto &element: vec) {
+        vec_rescaled.emplace_back(element * scale);
+    }
+    return vec_rescaled;
+}
+
+vecd normalize(const vecd &vec) {
+    double vec_norm = norm(vec);
+    if (vec_norm > 0) {
+        return scale(vec, 1 / vec_norm);
+    } else {
+        return vec;
+    }
+}
+
+veci valtovac(const vali &vec) {
+    return {std::begin(vec), std::end(vec)};
+}
+
+vecd valtovac(const vald &vec) {
+    return {std::begin(vec), std::end(vec)};
 }
 
