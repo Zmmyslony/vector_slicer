5916051aef32b83295a640be7b448e5ac444d428
zmmyslony <zmmyslony@gmail.com>
2024-04-11 15:28:02 +0100

    Added auto installation of numpy and scipy on macOS.

--- install_mac.sh
+++ install_mac.sh
@@ -2,7 +2,7 @@
 brew update
 brew upgrade --no-dry-run
 brew install llvm cmake git boost libomp
-brew install python-matplotlib
+brew install numpy scipy python-matplotlib
 
 git pull
 cmake -S ./ -B ./build -DCMAKE_C_COMPILER=$(brew --prefix llvm)/bin/clang -DCMAKE_CXX_COMPILER=$(brew --prefix llvm)/bin/clang++
