f157300cd0f4807ef72ff0b0f0c3e0db96f3d51d
zmmyslony <zmmyslony@gmail.com>
2024-04-17 15:25:58 +0100

    Added automatic export of LLVM and libomp flags on macOS.

--- install_mac.sh
+++ install_mac.sh
@@ -3,8 +3,13 @@ brew update
 brew upgrade
 brew install llvm cmake git boost libomp
 
-export LDFLAGS=""
-export CXXFLAGS=""
+# Export LLVM flags
+export LDFLAGS=$(brew --prefix llvm)/lib
+export CPPFLAGS=$(brew --prefix llvm)/include
+
+#Export libomp
+export LDFLAGS=$(brew --prefix libomp)/lib
+export CPPFLAGS=$(brew --prefix libomp)/include
 
 git pull
 cmake -S ./ -B ./build -DCMAKE_C_COMPILER=$(brew --prefix llvm)/bin/clang -DCMAKE_CXX_COMPILER=$(brew --prefix llvm)/bin/clang++
