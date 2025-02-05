0f203a942783ba33edb422b12064c7daeee1b6a1
zmmyslony <zmmyslony@gmail.com>
2025-02-05 12:24:55 +0000

    Added instructions on how to add the environment variables after the installation the GCode generator and the python module work correctly.

--- install_lin.sh
+++ install_lin.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
 sudo apt install cmake libboost-all-dev
 
 git pull
 cmake -S ./ -B ./build
 cmake --build ./build --config Release -j4
+
+echo
+echo
+echo "For automatic detection of the api location please add to your ~/.bashrc following lines:"
+echo "    export VECTOR_SLICER_OUTPUT=\"$PWD/output\""
+echo "    export VECTOR_SLICER_API=\"$PWD/build/libvector_slicer_api.so\""
+echo
 
 
--- install_mac.sh
+++ install_mac.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-brew update
-brew upgrade
+
 brew install llvm cmake git boost libomp
 
 # Export LLVM flags
@@ -14,3 +13,10 @@ export CPPFLAGS=$(brew --prefix libomp)/include
 git pull
 cmake -S ./ -B ./build -DCMAKE_C_COMPILER=$(brew --prefix llvm)/bin/clang -DCMAKE_CXX_COMPILER=$(brew --prefix llvm)/bin/clang++
 cmake --build ./build --config Release -j4
+
+echo
+echo
+echo "For automatic detection of the api location please add to your ~/.bashrc following lines:"
+echo "    export VECTOR_SLICER_OUTPUT=\"$PWD/output\""
+echo "    export VECTOR_SLICER_API=\"$PWD/build/libvector_slicer_api.dylib\""
+echo
