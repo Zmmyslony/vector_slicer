a0915fa013d235822b153248fdb9df2bbc4210d8
zmmyslony <zmmyslony@gmail.com>
2025-07-16 21:02:58 +0100

    Installation update

--- install_win.bat
+++ install_win.bat
@@ -6,23 +6,29 @@ set pwd=%mypath:~0,-1%
 IF EXIST "C:\src\vcpkg\" (
     cd C:\src\vcpkg\
     git pull
-    vcpkg install boost:x64-windows
-    vcpkg integrate install
 ) else (
      mkdir C:\src
      cd C:\src
      git clone https://github.com/Microsoft/vcpkg.git
      cd vcpkg && bootstrap-vcpkg.bat
-     vcpkg install boost:x64-windows
-     vcpkg integrate install
 )
+
+vcpkg install boost:x64-windows
+vcpkg integrate install
 
 set cmake_path="C:/src/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
 echo Installing Vector Slicer
 cd %pwd%
 
-git pull
+IF EXIST ".git" (
+    git pull
+) else (
+    echo Warning: Git directory does not exist, which will prevent you from easily updating the project in the future.
+    echo Please consider cloning the GitHub repository, by going into the desired parent directory and running:
+    echo git clone https://github.com/Zmmyslony/vector_slicer.git
+)
+
 cmake -S ./ -B ./build -DCMAKE_TOOLCHAIN_FILE=%cmake_path%
 cmake --build ./build --config Release -j4
 
