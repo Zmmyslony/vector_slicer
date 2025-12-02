f5053c8b6525979fce146f712e922533514db002
zmmyslony <zmmyslony@gmail.com>
2025-12-02 09:28:53 +0000

    Increased C and C++ stnadrd to 17, and removed the LLVM enforcement on MacOS

--- install_mac.sh
+++ install_mac.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-brew install llvm cmake git boost libomp
-
-# Export LLVM flags
-export LDFLAGS=$(brew --prefix llvm)/lib
-export CPPFLAGS=$(brew --prefix llvm)/include
-
-#Export libomp
-export LDFLAGS=$(brew --prefix libomp)/lib
-export CPPFLAGS=$(brew --prefix libomp)/include
+brew install cmake git boost libomp
 
 git pull
-cmake -S ./ -B ./build -DCMAKE_C_COMPILER=$(brew --prefix llvm)/bin/clang -DCMAKE_CXX_COMPILER=$(brew --prefix llvm)/bin/clang++
+cmake -S ./ -B ./build
 cmake --build ./build --config Release -j4
 
 echo
